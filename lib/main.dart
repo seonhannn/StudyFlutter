09c35b97f6d769d878ed299d418c00a5d6d585ee
seonhannn <seon.hannn@gmail.com>
2022-12-10 22:06:13 +0900

    coding apple 과제3 custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,38 +5,87 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("과제2"),
+          title: Text("과제3"),
         ),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 200,
-                height: 200,
-                child: Image.asset("assets/apples.jpg")
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("사과 팝니다."),
-                  Text("수원역 직거래"),
-                  Text("5000원"),
-                ],
-              )
-            ],
-          ),
-        )
+        body: ListView(
+          padding: EdgeInsets.all(20),
+          children: [
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+            People(),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: BottomBar(),
+        ),
+      ),
+    );
+  }
+}
+
+class People extends StatelessWidget {
+  const People({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Icon(Icons.people),
+        Text("한지선"),
+      ],
+    );
+  }
+}
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Icon(Icons.call),
+          Icon(Icons.message),
+          Icon(Icons.mail),
+        ],
       ),
     );
   }
