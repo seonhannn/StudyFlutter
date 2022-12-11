250e723ee7884ed058e677f0447fdb6fb05b682c
seonhannn <seon.hannn@gmail.com>
2022-12-11 13:08:05 +0900

    state 전달

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
-var likeNum = [0, 0, 0];
-var name = ["지선1", "지선2", "지선3"];
+var testText = "test!";
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("과제3"),
-            ),
-            body: Like()
-        )
+      home: Scaffold(
+        appBar: AppBar(title: const Text('AlertDialog Sample')),
+        body: const Center(
+          child: Test(testText: "test!")
+        ),
+      ),
     );
   }
 }
 
-class Like extends StatefulWidget {
-  const Like({Key? key}) : super(key: key);
+class Test extends StatelessWidget {
+  const Test({Key? key, this.testText}) : super(key: key);
+  final testText;
 
-  @override
-  State<Like> createState() => _LikeState();
-}
-
-class _LikeState extends State<Like> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: 3,
-        itemBuilder: (c, i) {
-          return ListTile(
-            leading: Text(likeNum[i].toString()),
-            title: Text(name[i]),
-            trailing: TextButton(
-              child: Text("좋아요"),
-              onPressed: () {
-                setState(() {
-                  likeNum[i]++;
-                });
-              },
-            ),
-          );
-        }
+    return Column(
+        children: [
+          Text("state 전달 테스트"),
+          Text(testText.toString()),
+        ],
     );
   }
 }
-
-
