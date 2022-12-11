6edc2a974c3c5156e4bdb83d4d6a78d84a47d477
seonhannn <seon.hannn@gmail.com>
2022-12-11 10:28:43 +0900

    coding apple 과제4 stateful

--- lib/main.dart
+++ lib/main.dart
@@ -4,89 +4,53 @@ void main() {
   runApp(const MyApp());
 }
 
+var likeNum = [0, 0, 0];
+var name = ["지선1", "지선2", "지선3"];
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("과제3"),
-        ),
-        body: ListView(
-          padding: EdgeInsets.all(20),
-          children: [
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-            People(),
-          ],
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: BottomBar(),
-        ),
-      ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("과제3"),
+            ),
+            body: Like()
+        )
     );
   }
 }
 
-class People extends StatelessWidget {
-  const People({Key? key}) : super(key: key);
+class Like extends StatefulWidget {
+  const Like({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Icon(Icons.people),
-        Text("한지선"),
-      ],
-    );
-  }
+  State<Like> createState() => _LikeState();
 }
 
-class BottomBar extends StatelessWidget {
-  const BottomBar({Key? key}) : super(key: key);
-
+class _LikeState extends State<Like> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Icon(Icons.call),
-          Icon(Icons.message),
-          Icon(Icons.mail),
-        ],
-      ),
+    return ListView.builder(
+        itemCount: 3,
+        itemBuilder: (c, i) {
+          return ListTile(
+            leading: Text(likeNum[i].toString()),
+            title: Text(name[i]),
+            trailing: TextButton(
+              child: Text("좋아요"),
+              onPressed: () {
+                setState(() {
+                  likeNum[i]++;
+                });
+              },
+            ),
+          );
+        }
     );
   }
 }
+
+
